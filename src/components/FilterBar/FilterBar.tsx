0bc0a1978f2a6c6f99012b9820caabbfc71cd1ea
j-novaes <joaovictor.nb21@hotmail.com>
2022-03-17 16:16:01 +0000

    Edit FilterBar and StyledFilterBar

--- src/components/FilterBar/FilterBar.tsx
+++ src/components/FilterBar/FilterBar.tsx
@@ -1,15 +1,30 @@
 import React from 'react'
 
+// FontAwesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
 //Styled Component
 import { StyledFilterBar } from './StyledFilterBar'
 
 export default function FilterBar() {
     return (
         <StyledFilterBar>
-            <input type='text'/>
+            <section className='search-bar'>
+                <FontAwesomeIcon className='search-icon' icon={faMagnifyingGlass} />
+                <input
+                    className='search-input'
+                    type='text'
+                    placeholder='Search for a country...'
+                />
+            </section>
 
-            <select>
-                <option value="" disabled selected hidden>Filter by Region</option>
+            <select
+                className='region-select'
+                defaultValue={''}
+            >
+                <option value="" disabled hidden>Filter by Region</option>
                 <option value='africa'>Africa</option>
                 <option value='america'>America</option>
                 <option value='asia'>Asia</option>
--- src/components/FilterBar/StyledFilterBar.ts
+++ src/components/FilterBar/StyledFilterBar.ts
@@ -5,5 +5,53 @@ export const StyledFilterBar = styled.section`
     display: flex;
     justify-content: space-between;
     align-items: center;
-    padding: 0px 30px;
+    padding: 30px 60px;
+
+    .search-bar {
+        width: 350px;
+        padding: 5px 0px;
+        display: flex;
+        border-radius: 4px;
+        overflow: hidden;
+        justify-content: center;
+        align-items: center;
+        background: hsl(209, 23%, 22%);
+
+        .search-icon {
+            color: hsl(0, 0%, 100%);
+            margin: 0px  10px 0px 20px;
+        }
+
+        .search-input {
+            width: 100%;
+            padding: 10px 10px;
+            border: none;
+            background: none;
+            color: hsl(0, 0%, 100%);
+            font-size: 14px;
+            font-weight: 600;
+            
+            &::placeholder {
+                font-size: 14px;
+                color: hsl(0, 0%, 100%);
+                font-weight: 600;
+            }
+    
+            &:focus {
+                outline: none;
+            }
+        }
+    }
+
+    .region-select {
+        background: hsl(209, 23%, 22%);
+        padding: 10px 15px;
+        border-radius: 4px;
+        color: hsl(0, 0%, 100%);
+        font-weight: 600;
+
+        &:focus {
+            outline: none;
+        }
+    }
 `
