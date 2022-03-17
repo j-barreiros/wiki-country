0703384ed4799e6b3024dc636f6bec59e62ce83e
j-novaes <joaovictor.nb21@hotmail.com>
2022-03-17 15:20:12 +0000

    Edit TopBar and StyledTopBar

--- src/components/TopBar/StyledTopBar.ts
+++ src/components/TopBar/StyledTopBar.ts
@@ -2,8 +2,25 @@ import styled from "styled-components";
 
 export const StyledTopBar = styled.section`
     width: 100%;
+    padding: 14px 60px 17px 60px;
     display: flex;
     justify-content: space-between;
     align-items: center;
-    padding: 0px 30px;
+    background: hsl(209, 23%, 22%);
+
+    h1, .theme-button {
+        color: hsl(0, 0%, 100%);
+    }
+
+    .theme-button {
+        background: none;
+        border:none;
+        font-size: 15px;
+        font-weight: 600;
+        cursor: pointer;
+        transition: all 0.5s;
+
+        &:hover {
+        }
+    }
 `
--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
 
+// FontAwesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { faMoon as faMoomSolid } from "@fortawesome/free-solid-svg-icons";
+import { faMoon as faMoonRegular } from "@fortawesome/free-regular-svg-icons";
+
 // Styled Component
 import { StyledTopBar } from './StyledTopBar';
 
@@ -10,7 +16,14 @@ export default function TopBar() {
     return (
         <StyledTopBar>
             <h1>Where in the world?</h1>
-            <button>{ isInDarkMode ? 'Light Mode' : 'Dark Mode'}</button>
+            <button
+                className='theme-button'
+                // Put change theme function here
+                onClick={() => {}}
+            >
+                <FontAwesomeIcon icon={isInDarkMode ? faMoomSolid : faMoonRegular} />
+                Dark Mode
+            </button>
         </StyledTopBar>
     )
 }
